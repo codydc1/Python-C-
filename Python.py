0959b2470c07317a0e322aa7517aae1f640cf849
codydc1 <80163693+codydc1@users.noreply.github.com>
2023-10-18 19:05:04 -0400

    Create Python.py

---
+++ Python.py
@@ -0,0 +1,22 @@
+import re
+import string
+
+
+def printsomething():
+    print("Hello from python!")
+
+def PrintMe(v):
+    print("You sent me: " + v)
+    return 100;
+
+def SquareValue(v):
+    return v * v
+
+def MultiplicationTable(n):
+
+    for i in range(1,11):
+        print(n,'X',i,'=',n*i)
+    return 0;
+
+def DoubleValue(n):
+    return n*2
